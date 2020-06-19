e652942d399ad7f6c13333e24d2fc71c587f9305
miso <nightmin734175@yahoo.com>
2020-06-19 11:49:51 +0800

    initial commit

--- workgame/new.py
+++ workgame/new.py
@@ -1,43 +1,24 @@
 import sys
-import pygame
+import pygame as pygame
 from pygame.locals import QUIT
 from pygame.locals import *
 
-class Dessert:
-    def _init_self(self, fname):
-        self._image = pygame.image.load(fname)
-        self._box = self._image.getRect()
 
-def check_rect_collide0(rect_blackbear1, dessert_box[0])bool:
-    if rect_blackbear1.bottom > dessert_box[0].top and 
-       rect_blackbear1.top < dessert_box[0].bottom and 
-       rect_blackbear1.right > dessert_box[0].left and 
-       rect_blackbear1.left < dessert_box[0].right:
-        return deadthing1 += 1
+
+def check_rect_collide0(rect_blackbear1, dessert_box):
+    if (rect_blackbear1.bottom > dessert_box.top and 
+        rect_blackbear1.top < dessert_box.bottom and 
+        rect_blackbear1.right > dessert_box.left and 
+        rect_blackbear1.left < dessert_box.right):
+        return deadthing1 + 1 and dessert.remove(dessert[i])
     return null
 
-def check_rect_collide1(rect_blackbear1, dessert_box[1]):
-    if rect_blackbear1.bottom > dessert_box[1].top and 
-       rect_blackbear1.top < dessert_box[1].bottom and 
-       rect_blackbear1.right > dessert_box[1].left and 
-       rect_blackbear1.left < dessert_box[1].right:
-        return deadthing1 += 1
-    return null
-
-def check_rect_collide2(rect_blackbear1, dessert_box[2]):
-    if rect_blackbear1.bottom > dessert_box[2].top and 
-       rect_blackbear1.top < dessert_box[2].bottom and 
-       rect_blackbear1.right > dessert_box[2].left and 
-       rect_blackbear1.left < dessert_box[2].right:
-        return deadthing1 += 1
-    return null
-
-def check_rect_collide3(rect_blackbear2, dessert_box[3]):
-    if rect_blackbear2.bottom > dessert_box[3].top and 
-       rect_blackbear2.top < dessert_box[3].bottom and 
-       rect_blackbear2.right > dessert_box[3].left and 
-       rect_blackbear2.left < dessert_box[3].right:
-        return deadthing2 += 1
+def check_rect_collide1(rect_blackbear2, dessert_box):
+    if (rect_blackbear2.bottom > dessert_box.top and 
+        rect_blackbear2.top < dessert_box.bottom and 
+        rect_blackbear2.right > dessert_box.left and 
+        rect_blackbear2.left < dessert_box.right):
+        return deadthing2 + 1 and dessert.remove(dessert[3])
     return null
 
 def main():
@@ -67,13 +48,13 @@ def main():
   
     blackbear1 = pygame.image.load("resources/png/blackbear1.png")
     blackbear1.convert()
-    rect_blackbear1 = blackbear1.getRect()
+    rect_blackbear1 = blackbear1.get_rect()
     x4 = 50
     y4 = 500
     
     blackbear2 = pygame.image.load("resources/png/blackbear2.png")
     blackbear2.convert()
-    rect_blackbear2 = blackbear2.getRect()
+    rect_blackbear2 = blackbear2.get_rect()
     x5 = 50
     y5 = 500
     
@@ -84,10 +65,7 @@ def main():
     
     for i in range(4):
         dessert[i] = pygame.image.load(dessert_name)
-        dessert_box[i] = dessert[i].getRect()
-            
-    for i in range(4):
-        dessert[i] = Dessert(dessert_name[i])
+        dessert_box[i] = dessert[i].get_rect()
         dessert_xy[i] = dessert[i]
     
     end1 = pygame.image.load("resources/png/yes.png")
@@ -101,7 +79,7 @@ def main():
     y7 = 150
     
     if round == 2:
-        if life2 = True:
+        if life2 == True:
             win.fill((255, 255, 255))   
             round_surface = head_font.render('Round2', True, (0, 0, 0))  
             win.blit(round_surface, (300, 100))   
@@ -111,8 +89,10 @@ def main():
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
             
-            while event.type == KEYDOWN:
+            if event.type == KEYDOWN:
                 if event.key == K_LEFT:
                     x4 += -50
                     x5 += -50
@@ -151,21 +131,23 @@ def main():
                     y1 += 10
                     x2 += -10
                     y2 += -10                
-                    for i in range(4):
+                    for i in range(3):
                         win.fill((255, 255, 255))
                         win.blit(blackbear1, (x4, y4))
                         win.blit(dessert[i], dessert_xy[i])
                         pygame.display.update()
                         pygame.time.wait(500)
                     check_rect_collide0(rect_blackbear1, dessert_box[0])
-                    check_rect_collide1(rect_blackbear1, dessert_box[1])
-                    check_rect_collide2(rect_blackbear1, dessert_box[2])                    
+                    check_rect_collide0(rect_blackbear1, dessert_box[1])
+                    check_rect_collide0(rect_blackbear1, dessert_box[2])                    
                     if deadthing1 == 3:
+                        win.fill((255, 255, 255))
+                        pygame.display.update()
                         life1 == False
                         round == 2 
                   
             elif round == 2:
-                if life2 = True:
+                if life2 == True:
                     if x3 < 50 or x3 > 750:
                         x3 = 700
                     if y3 < 50 or y3 > 550:
@@ -175,12 +157,16 @@ def main():
                     win.blit(dessert[3], dessert_xy[3])
                     pygame.display.update()
                     pygame.time.wait(500)
-                    check_rect_collide3(rect_blackbear2, dessert_box[3])
+                    check_rect_collide1(rect_blackbear2, dessert_box[3])
                     if deadthing2 == 1:
+                        win.fill((255, 255, 255))
+                        pygame.display.update()
                         life2 == False
                         End == True
                         end == 1   
                     else:
+                        win.fill((255, 255, 255))
+                        pygame.display.update()
                         End == True
                         end == 2
             else:
